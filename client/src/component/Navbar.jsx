7dcbf064e7d883caf4b55ddc29e15abb3a064cee
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-07-27 22:33:45 +0530

    updaate

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const AppNavbar = () => {
+  const navLinkStyle = {
+    marginLeft: '1rem',  // Space between links
+    color: '#000',       // Default link color
+    textDecoration: 'none' // Remove underline
+  };
+
+  const navLinkHoverStyle = {
+    color: '#007bff' // Color on hover
+  };
+
   return (
     <Navbar expand="lg" className="mb-4">
       <div className="container">
@@ -10,9 +20,30 @@ const AppNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link to="/create">Create URL</Link>
-            <Link  to="/list">List URLs</Link>
-            <Link  to="/login">Logout</Link>
+            <Link 
+              to="/create" 
+              style={navLinkStyle} 
+              onMouseOver={(e) => e.currentTarget.style.color = navLinkHoverStyle.color}
+              onMouseOut={(e) => e.currentTarget.style.color = navLinkStyle.color}
+            >
+              Create URL
+            </Link>
+            <Link 
+              to="/list" 
+              style={navLinkStyle} 
+              onMouseOver={(e) => e.currentTarget.style.color = navLinkHoverStyle.color}
+              onMouseOut={(e) => e.currentTarget.style.color = navLinkStyle.color}
+            >
+              List URL
+            </Link>
+            <Link 
+              to="/login" 
+              style={navLinkStyle} 
+              onMouseOver={(e) => e.currentTarget.style.color = navLinkHoverStyle.color}
+              onMouseOut={(e) => e.currentTarget.style.color = navLinkStyle.color}
+            >
+              Logout
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </div>
